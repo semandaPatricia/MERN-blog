7ded7888b7ddcd6622512d1ef401c79312c8b0af
semanda <patash248@gmail.com>
2024-01-30 16:55:05 +0100

    update Header components with the link paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,30 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/Dashboard';
-import Search from './pages/Search';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Search from "./pages/Search";
+import Header from "./components/Header";
 
 //import './App.css'
 
 function App() {
-  
-
   return (
-   <BrowserRouter>
-   <Header />
+    <BrowserRouter>
+      <Header />
 
-   <Routes>
-   <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/search' element={<Search />} />
-        
-        <Route path='/dashboard' element={<Dashboard />} />
-   </Routes>
-   </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
